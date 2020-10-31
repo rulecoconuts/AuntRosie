300cce7252f09a5f53280d2b39c030da5113f830
rulecoconuts <rulecoconuts@gmail.com>
2020-10-31 07:47:04 -0400

    Wrote SQL for the products, product items, product sizes, product  nutritions and nutritions tables

---
+++ AuntRosieUserInterface/DB/dbo.tblProducts.sql
@@ -0,0 +1,8 @@
+﻿create table [dbo].[tblProducts]
+(
+   [ProductID] int primary key not null,
+   [ProductName] varchar not null,
+   [ProductType] varchar not null,
+   [ServingSize] float not null,
+   [Unit] varchar not null
+);
