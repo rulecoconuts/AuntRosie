c64a79ad253db6c57c04aee9a49d673783aea4f1
rolando-agullanojr <43216288+rolando-agullanojr@users.noreply.github.com>
2020-11-04 08:49:09 -0500

    Added controllers for each table entity.
    Added interfaces to manipulate the basic CRUD structure for each class.

---
+++ AuntRosieUserInterface/Classes/Ingredient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuntRosieUserInterface.Classes
+{
+    class Ingredient
+    {
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        Ingredient() { }
+
+        #region CRUD Structure
+
+        /// <summary>
+        /// Creates a new product object
+        /// </summary>
+        /// <returns></returns>
+        bool Create() { return true; }
+
+        /// <summary>
+        /// Reads an object from the database
+        /// </summary>
+        /// <returns></returns>
+        bool Read() { return true; }
+
+        /// <summary>
+        /// Updates an object from the database
+        /// </summary>
+        /// <returns></returns>
+        bool Update() { return true; }
+
+
+        /// <summary>
+        /// Deletes an object from the database
+        /// </summary>
+        /// <returns></returns>
+        bool Delete() { return true; }
+
+        #endregion
+    }
+}
