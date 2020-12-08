f7ad46918ebf3879ab5ba15db51717130a1d84e1
rolando-agullanojr <43216288+rolando-agullanojr@users.noreply.github.com>
2020-12-08 10:04:57 -0500

    Part 2

---
+++ AuntRosieApplication/Reports/ReportVewierForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuntRosieApplication
+{
+    public partial class ReportVewierForm : Form
+    {
+        private ReportDataSet aDataSet;
+        //table adapter object
+        private ReportDataSetTableAdapters.StockReportTableAdapter aStockAdapter;
+        private ReportDataSetTableAdapters.LowStockTableAdapter aLowStockAdapter;
+        private ReportDataSetTableAdapters.EmpEventTableAdapter aEmpEventAdapter;
+        private ReportDataSetTableAdapters.ChangePriceTableAdapter aChangePriceAdapter;
+
+        public ReportVewierForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportVewierForm_Load(object sender, EventArgs e)
+        {
+            if (Classes.DBMethod.Reptype == "All-Stock")
+            {
+                LoadStockRep();
+            }
+            else if (Classes.DBMethod.Reptype == "Low-Stock")
+            {
+                LoadLowStockRep();
+
+            }
+            else if (Classes.DBMethod.Reptype == "Emp-Event")
+            { LoadEmpEvent(); }
+            else if (Classes.DBMethod.Reptype == "Diff-Price")
+            { LoadDiffPrice(); }
+        }
+        private void  LoadStockRep()
+        {
+            StockReport astockRep = new StockReport();
+
+            try
+            {
+
+                //Instantiate the dataset and table adapters
+                aDataSet = new ReportDataSet();
+                aStockAdapter = new ReportDataSetTableAdapters.StockReportTableAdapter();
+                //Fill the dataset 
+                //aStockAdapter.Adapter.SelectCommand = new System.Data.SqlClient.SqlCommand(AuntRosieApplication.Inventory.frmInventoryStock.reportSql);
+
+
+                aStockAdapter.Fill(aDataSet.StockReport);
+                //Assign the filled dataset as the data source for the report
+                astockRep.SetDataSource(aDataSet);
+                //Set up the report viewer object on the form
+                //show the runtime report object
+                crvReports.ReportSource = astockRep;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch an exception thrown during data object intonation
+                //or report generation and display based on the dataset
+                MessageBox.Show("Data Error Encountered" + ex.ToString(), "ERROR");
+            }
+        }
+
+        private void LoadLowStockRep()
+        {
+            LowStockRep aLowStockRep = new LowStockRep();
+
+            try
+            {
+
+                //Instantiate the dataset and table adapters
+                aDataSet = new ReportDataSet();
+                aLowStockAdapter = new ReportDataSetTableAdapters.LowStockTableAdapter();
+                //Fill the dataset 
+               // aStockAdapter.Adapter.SelectCommand = new System.Data.SqlClient.SqlCommand(AuntRosieApplication.Inventory.frmInventoryStock.reportSql);
+
+
+                aLowStockAdapter.Fill(aDataSet.LowStock);
+                //Assign the filled dataset as the data source for the report
+                aLowStockRep.SetDataSource(aDataSet);
+                //Set up the report viewer object on the form
+                //show the runtime report object
+                crvReports.ReportSource = aLowStockRep;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch an exception thrown during data object intonation
+                //or report generation and display based on the dataset
+                MessageBox.Show("Data Error Encountered" + ex.ToString(), "ERROR");
+            }
+
+
+        }
+        private void LoadEmpEvent()
+        {
+
+            EmpEventRep aEmpEventRep = new EmpEventRep();
+
+            try
+            {
+
+                //Instantiate the dataset and table adapters
+                aDataSet = new ReportDataSet();
+                aEmpEventAdapter = new ReportDataSetTableAdapters.EmpEventTableAdapter();
+                //Fill the dataset 
+                   aEmpEventAdapter.Fill(aDataSet.EmpEvent);
+                //Assign the filled dataset as the data source for the report
+                aEmpEventRep.SetDataSource(aDataSet);
+                //Set up the report viewer object on the form
+                //show the runtime report object
+                crvReports.ReportSource = aEmpEventRep;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch an exception thrown during data object intonation
+                //or report generation and display based on the dataset
+                MessageBox.Show("Data Error Encountered" + ex.ToString(), "ERROR");
+            }
+
+
+        }
+        private void LoadDiffPrice()
+        {
+
+            ChanagepriceRep aChangePriceRep = new ChanagepriceRep();
+
+            try
+            {
+
+                //Instantiate the dataset and table adapters
+                aDataSet = new ReportDataSet();
+                aChangePriceAdapter = new ReportDataSetTableAdapters.ChangePriceTableAdapter();
+
+                aChangePriceAdapter.Fill(aDataSet.ChangePrice);
+                //Assign the filled dataset as the data source for the report
+                aChangePriceRep.SetDataSource(aDataSet);
+                //Set up the report viewer object on the form
+                //show the runtime report object
+                crvReports.ReportSource = aChangePriceRep;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch an exception thrown during data object intonation
+                //or report generation and display based on the dataset
+                MessageBox.Show("Data Error Encountered" + ex.ToString(), "ERROR");
+            }
+
+
+
+
+        }
+        private void crvReports_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ AuntRosieApplication/Reports/ReportsForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AuntRosieApplication.Classes;
+namespace AuntRosieApplication.Reports
+{
+    public partial class frmReports : Form
+    {
+        public frmReports()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e) { }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+
+        private void btnLowStock_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmReports_Load(object sender, EventArgs e)
+        {
+            lblTitle.Left = (this.Width - lblTitle.Width) / 2;
+            this.BackgroundImage = global::AuntRosieApplication.Properties.Resources.background2;
+
+        }
+
+        private void btnLowStock_Click_1(object sender, EventArgs e)
+        {
+ DBMethod.Reptype = "Low-Stock";
+            (new ReportVewierForm()).ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChangeIngredientPrice_Click(object sender, EventArgs e)
+        {
+            DBMethod.Reptype = "Diff-Price";
+            (new ReportVewierForm()).ShowDialog();
+        }
+
+        private void btnNutrition_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEmployeeEvent_Click(object sender, EventArgs e)
+        {
+            DBMethod.Reptype = "Emp-Event";
+            (new ReportVewierForm()).ShowDialog();
+        }
+    }
+}
