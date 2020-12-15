1c9991783206b38cf80b2b7db04762fa57972c47
rulecoconuts <rulecoconuts@gmail.com>
2020-12-15 00:18:25 -0500

    Wrote method in ProductIngredient class to get a datatable containing the ingredients of a supplied product item. Also started coding the form to manage product ingredients

---
+++ AuntRosieApplication/Kitchen/ManageProductIngredients.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AuntRosieEntities;
+
+namespace AuntRosieApplication.Kitchen
+{
+    public partial class frmManageProductIngredients : Form
+    {
+        private BindingSource ingredientSource = new BindingSource();
+        public frmManageProductIngredients()
+        {
+            InitializeComponent();
+        }
+
+        private void frmManageProductIngredients_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = global::AuntRosieApplication.Properties.Resources.background2;
+            String DatabasePath = System.IO.Directory.GetCurrentDirectory();
+            int x = DatabasePath.IndexOf("bin");
+            DatabasePath = DatabasePath.Substring(0, x - 1);
+            String conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + DatabasePath +
+              @"\AuntRosieDB.mdf;Integrated Security=True;Connect Timeout=30";
+            DBConnector conn = new DBConnector(conStr);
+            RosieEntity.Connector = conn;
+            prepareDataGrid();
+        }
+
+        /// <summary>
+        /// Prepare/setup data grid view
+        /// </summary>
+        private void prepareDataGrid()
+        {
+            addUpdateButtonColumn();
+            dtgIngredients.DataSource = ingredientSource;
+            dtgIngredients.AutoGenerateColumns = true;
+        }
+
+        /// <summary>
+        /// Add column that contains update buttons to the
+        /// production data grid view
+        /// </summary>
+        private void addUpdateButtonColumn()
+        {
+            DataGridViewButtonColumn updateBtnColumn = new DataGridViewButtonColumn();
+            updateBtnColumn.HeaderText = "Update";
+            updateBtnColumn.Name = "updateColumn";
+            updateBtnColumn.Text = "Update";
+            updateBtnColumn.UseColumnTextForButtonValue = true;
+            dtgIngredients.Columns.Add(updateBtnColumn);
+        }
+    }
+}
--- AuntRosieEntities/ProductIngredient.cs
+++ AuntRosieEntities/ProductIngredient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -29,19 +30,56 @@ namespace AuntRosieEntities
             }
         }
 
+        public int ProductItemId { get => productItemId; set => productItemId = value; }
+        public long IngredientId { get => ingredientId; set => ingredientId = value; }
+        public double Quantity { get => quantity; set => quantity = value; }
+        public string Unit { get => unit; set => unit = value; }
+
         public override void Create(SqlTransaction transaction = null)
         {
-            throw new NotImplementedException();
+            string query = "insert into [tblProductIngredient]([ProductItemID], [IngredientID], [Quantity], [Unit]) " +
+                $"values ({ProductItemId}, {IngredientId}, {Quantity}, '{Unit}')";
+            Connector.Insert(query, false, transaction);
         }
 
         public override void Delete(SqlTransaction transaction = null)
         {
-            throw new NotImplementedException();
+            string query = $"delete from [tblProductIngredient] where " +
+                $"[ProductItemID]={ProductItemId} and [IngredientID]={IngredientId}";
+            Connector.Delete(query);
         }
 
         public override void Update(SqlTransaction transaction = null)
         {
-            throw new NotImplementedException();
+            string query = $"update [tblProductIngredient] set [Quantity]={Quantity}, [Unit]='{Unit}'";
+            Connector.Update(query, transaction);
+        }
+
+        public static DataTable GetIngredientsTable(int productItemID)
+        {
+            DataTable ingredients = new DataTable();
+            string query = "select p.[ProductName] as 'Product Name', concat(psz.[SizeName], '(', psz.[SizeValue], ' ', " +
+                "psz.[Unit], ')') as 'Size', i.[IngredientName] as 'Ingredient Name', it.[TypeName] as 'Type' " +
+                "from [tblProductIngredient] pin " +
+                "inner join [tblProductItem] pi on pin.[ProductItemID]=pi.[ProductItemID] " +
+                "inner join [tblProduct] p on p.[ProductID] = pi.[ProductID] " +
+                "inner join [tblProductSize] psz on psz.[SizeID] = pi.[SizeID] " +
+                "inner join [tblIngredient] i on i.[IngredientID]=pin.[IngredientID] " +
+                "inner join [tblIngredientType] it on i.[IngredientTypeID]=it.[IngredientTypeID] " +
+                $"where pin.[ProductItemID]={productItemID}";
+
+            SqlCommand command = new SqlCommand(query, Connector.Connection);
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(ingredients);
+            }
+            catch
+            {
+                return null;
+            }
+
+            return ingredients;
         }
     }
 }
